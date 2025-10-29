d403125b0ceadda979d254095b1619ab591fa800
Zach Zundel <me@zachzundel.com>
2025-10-29 00:53:44 -0700

    chatgpt backend

--- src/models/chatgpt.rs
+++ src/models/chatgpt.rs
@@ -1,6 +1,11 @@
 use crate::models::model::Model;
-use anyhow::{Error, Result};
-use futures::stream::BoxStream;
+
+use anyhow::Result;
+use futures::stream::{BoxStream, StreamExt};
+use json;
+use reqwest_eventsource::{retry::Never, Error as RESError, Event, RequestBuilderExt};
+use std::collections::HashMap;
+use reqwest;
 
 pub struct ChatGPT {}
 
@@ -10,10 +15,53 @@ impl ChatGPT {
     }
 }
 
+
 impl<'a> Model<'a> for ChatGPT {
-    fn call(&self, _prompt: &str) -> Result<BoxStream<'a, String>> {
-        println!("Calling on ChatGPT");
-        Err(Error::msg("not implemented"))
+    fn call(&self, prompt: &str) -> Result<BoxStream<'a, String>> {
+        println!("oai");
+        let mut body = json::JsonValue::new_object();
+        body["model"] = "gpt-4.1".into();
+        body["max_output_tokens"] = 1024.into();
+        body["input"] = prompt.into();
+        body["stream"] = true.into();
+
+        let client = reqwest::Client::new();
+        let mut response = client
+            .post("https://api.openai.com/v1/responses")
+            .bearer_auth(env!("OPENAI_API_KEY"))
+            .header("content-type", "application/json")
+            .body(json::stringify(body))
+            .eventsource()?;
+
+        response.set_retry_policy(Box::new(Never {}));
+        Ok(response
+            .filter_map(|event| async move {
+                match event {
+                    Ok(Event::Open) => None,
+                    Ok(Event::Message(msg)) => Some(msg),
+                    Err(RESError::StreamEnded) => None,
+                    Err(e) => {
+                        eprintln!("{e:?}");
+                        None
+                    }
+                }
+            })
+            .filter_map(|event| async move {
+                match event.event.as_str() {
+                    "response.failed" => {
+                        eprintln!("Error event: {:?}", event.data);
+                        None
+                    }
+                    "response.output_text.delta" => Some(event.data),
+                    _ => None,
+                }
+            })
+            .map(|data| {
+                json::parse(&data)
+                    .map(|d| d["delta"].to_string())
+                    .unwrap_or(String::new())
+            })
+            .boxed())
     }
 
     fn name(&self) -> &str {
